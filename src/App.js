020ec76b79e113d0f10c1a7977b48f909a905bba
GabrielaParedes27 <78928248+GabrielaParedes27@users.noreply.github.com>
2021-08-10 20:32:14 -0500

    react tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { fill } from 'lodash';
+import { useState } from 'react';
 import './App.css';
+import Board from './components/Board/Board';
 
-function App() {
+const App = () => {
+
+  const [turn, setTurn] = useState('X');
+  const [squares, setSquares] = useState(Array(9),fill(null));
+  const [score, setScore] = useState({
+    X: 0,
+    O: 0,
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Board squares={squares}/>
     </div>
   );
 }
